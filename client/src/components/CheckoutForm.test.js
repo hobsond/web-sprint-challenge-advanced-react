51b9d22c4b1920389972a03f75d742c0a43f61cb
unknown <hobsond26@icloud.com>
2020-05-08 12:51:09 -0400

    finished step 2

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,28 @@
 import React from "react";
 import { render } from "@testing-library/react";
+
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    const {getByText}= render(<CheckoutForm/>)
+    const header = getByText(/checkout form/i)
+    expect(header).toBeTruthy()
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    const {container} = render(<CheckoutForm/>)
+    const testVars = {
+        firstName:'bryan',
+        lastName:'smith',
+        address:'1122 e palmbale',
+        city:'city of treason',
+        state:'va',
+        zip:'2223334'
+    }
+    const fname= container.querySelector('[name="firstName"] ')
+    expect(fname).toBeTruthy()
+
+
+});
